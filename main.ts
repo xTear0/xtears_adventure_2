d8add82049b809c4adf87e95d27b637d2bf4401e
Spencer Kotrosa <37277682+xTear0@users.noreply.github.com>
2026-07-11 14:17:35 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
+namespace SpriteKind {
+    export const Effect = SpriteKind.create()
+}
+function AddEffect (Duration: number, x: number, y: number) {
+    EffectSystem = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Effect)
+}
+function AddExplosion (ExplosionType: string, Size: number, Damage: number, x: number, y: number) {
+	
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (INPUT_MODE == "Game") {
         DoAction(STATE_CASTING)
@@ -8,6 +34,33 @@ scene.onHitWall(SpriteKind.Player, function (sprite, location) {
         RemoveState(STATE_JUMP)
     }
 })
+function AddEntity (ID: number, Location: tiles.Location) {
+    Entity = sprites.create(assets.image`constructArrays10`, SpriteKind.Enemy)
+    animation.runImageAnimation(
+    Entity,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    500,
+    false
+    )
+    tiles.placeOnTile(Entity, tiles.getTileLocation(Location.column, Location.row))
+}
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (INPUT_MODE == "Game") {
         DoAction(STATE_ULTIMATE)
@@ -18,6 +71,51 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         DoAction(STATE_JUMP)
     }
 })
+function StartingConstruction () {
+    INPUT_MODE = "Game"
+    STATE_JUMP = "Jump"
+    STATE_ATTACK = "Attack"
+    STATE_BLOCKING = "Blocking"
+    STATE_AIMING = "Aiming"
+    STATE_CASTING = "Casting"
+    STATE_ULTIMATE = "Ultimate"
+    STATE_IDLERUN = "IdleRun"
+    ENTITY_ANIM_IDLERUN = [[
+    assets.animation`mobAnimation`,
+    assets.animation`mobAnimation2`,
+    assets.animation`mobAnimation3`,
+    assets.animation`mobAnimation4`
+    ], [
+    assets.animation`mobAnimation`,
+    assets.animation`mobAnimation2`,
+    assets.animation`mobAnimation3`,
+    assets.animation`mobAnimation4`
+    ], [
+    assets.animation`mobAnimation`,
+    assets.animation`mobAnimation2`,
+    assets.animation`mobAnimation3`,
+    assets.animation`mobAnimation4`
+    ]]
+    ENTITY_ANIM_HURT = [[
+    assets.animation`mobAnimation`,
+    assets.animation`mobAnimation2`,
+    assets.animation`mobAnimation3`,
+    assets.animation`mobAnimation4`
+    ], [
+    assets.animation`mobAnimation`,
+    assets.animation`mobAnimation2`,
+    assets.animation`mobAnimation3`,
+    assets.animation`mobAnimation4`
+    ], [
+    assets.animation`mobAnimation`,
+    assets.animation`mobAnimation2`,
+    assets.animation`mobAnimation3`,
+    assets.animation`mobAnimation4`
+    ]]
+}
+function AddVectorFireball (Instigator: Sprite, Target: Sprite, AccuracyRange: number) {
+	
+}
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
     if (INPUT_MODE == "Game") {
         STATE_AIMING_DURATION = game.runtime() - STATE_AIMING_DURATION
@@ -26,6 +124,9 @@ controller.down.onEvent(ControllerButtonEvent.Released, function () {
 })
 function HasState (State: string) {
     return CharacterStates.indexOf(State) >= 0
+}
+function AddVectorEffect (Type: string, Instigator: Sprite, Target: Sprite, AccuracyRange: number) {
+	
 }
 function AddState (State: string, SelfMutex: boolean, MutexStates: string[], AddFirst: boolean) {
     if (HasState(STATE_IDLERUN)) {
@@ -96,6 +197,14 @@ function PlayCheckedTimedStateAnimation (AnimationDuration: number, RemoveState2
         true
         )
     }
+    if (CharacterStates[0] == STATE_ULTIMATE) {
+        animation.runImageAnimation(
+        Character,
+        assets.animation`player_ultimate`,
+        100,
+        true
+        )
+    }
     if (RemoveState2) {
         timer.after(AnimationDuration, function () {
             RemoveState(CharacterStates[0])
@@ -132,6 +241,12 @@ function CreatePlayerComponent () {
     175,
     true
     )
+    Character_Shield = sprites.create(assets.image`player_shield`, SpriteKind.Effect)
+    Character_Shield.setFlag(SpriteFlag.Invisible, true)
+    Character_Shield.setPosition(Character.x + 6, Character.y)
+    Character_Bow = sprites.create(assets.image`player_bow`, SpriteKind.Effect)
+    Character_Bow.setFlag(SpriteFlag.Invisible, true)
+    Character_Bow.setPosition(Character.x + 2, Character.y)
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (INPUT_MODE == "Game") {
@@ -159,10 +274,12 @@ function DoAction (Action: string) {
             AddState(STATE_AIMING, true, [STATE_BLOCKING, STATE_ATTACK, STATE_CASTING], true)
             STATE_AIMING_DURATION = game.runtime()
             PlayCheckedTimedStateAnimation(0, false)
+            AttachBow()
         }
         if (Action == STATE_BLOCKING) {
             AddState(STATE_BLOCKING, true, [STATE_ATTACK, STATE_AIMING, STATE_CASTING], true)
             PlayCheckedTimedStateAnimation(0, false)
+            AttachShield()
         }
         if (Action == STATE_JUMP) {
             if (Character.isHittingTile(CollisionDirection.Bottom)) {
@@ -180,31 +297,58 @@ function DoAction (Action: string) {
         STATE_BLOCKING,
         STATE_JUMP
         ], true)
+        PlayCheckedTimedStateAnimation(0, false)
     }
 }
+function AttachShield () {
+    Character_Shield.setFlag(SpriteFlag.Invisible, false)
+    timer.background(function () {
+        while (HasState(STATE_BLOCKING)) {
+            Character_Shield.setPosition(Character.x + 6, Character.y)
+            pause(10)
+        }
+        Character_Shield.setFlag(SpriteFlag.Invisible, true)
+    })
+}
+function AttachBow () {
+    Character_Bow.setFlag(SpriteFlag.Invisible, false)
+    animation.runImageAnimation(
+    Character_Bow,
+    assets.animation`playerBowAnim`,
+    200,
+    false
+    )
+    timer.background(function () {
+        while (HasState(STATE_AIMING)) {
+            Character_Bow.setPosition(Character.x - 1, Character.y)
+            pause(10)
+        }
+        Character_Bow.setFlag(SpriteFlag.Invisible, true)
+    })
+}
+let Character_Bow: Sprite = null
+let Character_Shield: Sprite = null
 let j = 0
 let CharacterStates: string[] = []
 let STATE_AIMING_DURATION = 0
-let Character: Sprite = null
+let ENTITY_ANIM_HURT: Image[][][] = []
+let ENTITY_ANIM_IDLERUN: Image[][][] = []
 let STATE_IDLERUN = ""
-let STATE_ULTIMATE = ""
-let STATE_CASTING = ""
 let STATE_AIMING = ""
 let STATE_BLOCKING = ""
 let STATE_ATTACK = ""
+let STATE_ULTIMATE = ""
+let Entity: Sprite = null
 let STATE_JUMP = ""
+let STATE_CASTING = ""
 let INPUT_MODE = ""
-INPUT_MODE = "Game"
-STATE_JUMP = "Jump"
-STATE_ATTACK = "Attack"
-STATE_BLOCKING = "Blocking"
-STATE_AIMING = "Aiming"
-STATE_CASTING = "Casting"
-STATE_ULTIMATE = "Ultimate"
-STATE_IDLERUN = "IdleRun"
+let EffectSystem: Sprite = null
+let Character: Sprite = null
+StartingConstruction()
 CreatePlayerComponent()
 tiles.setCurrentTilemap(tilemap`level1`)
 tiles.placeOnTile(Character, tiles.getTileLocation(2, 6))
+AddEntity(21, tiles.getTileLocation(8, 6))
 game.onUpdate(function () {
     // Debug display of the current state list
     Character.sayText(CharacterStates, 100, false)
